140af25811cdbc391debdaa01b1afe2c1f58a2be
suam4597-ship-it <suam4597@gmail.com>
2026-04-01 00:06:32 +0900

    feat(phase1): add disclosure_api test config

---
+++ apps/backend/disclosure_api/config/test.exs
@@ -0,0 +1,16 @@
+import Config
+
+config :disclosure_automation, DisclosureAutomation.Repo,
+  username: System.get_env("POSTGRES_USER") || "postgres",
+  password: System.get_env("POSTGRES_PASSWORD") || "postgres",
+  hostname: System.get_env("POSTGRES_HOST") || "localhost",
+  database: System.get_env("POSTGRES_TEST_DB") || "disclosure_automation_test",
+  pool: Ecto.Adapters.SQL.Sandbox,
+  pool_size: 5
+
+config :disclosure_automation, DisclosureAutomationWeb.Endpoint,
+  http: [ip: {127, 0, 0, 1}, port: 4002],
+  secret_key_base: "test-key",
+  server: false
+
+config :logger, level: :warning
