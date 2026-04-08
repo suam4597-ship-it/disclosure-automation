6d2b963b7ee3831e68f0f23763c4c1c0faa5c753
suam4597-ship-it <suam4597@gmail.com>
2026-04-08 15:19:19 +0900

    feat(sec): add adapter.ex from local workspace

---
+++ apps/backend/disclosure_api/lib/disclosure_automation/runtime/adapter.ex
@@ -0,0 +1,22 @@
+defmodule DisclosureAutomation.Runtime.Adapter do
+  @moduledoc false
+
+  alias DisclosureAutomation.Schema.SourceRegistry
+  alias DisclosureAutomation.Runtime.SECAdapter
+
+  @type source :: SourceRegistry.t()
+  @type discovery_item :: map()
+  @type hydrated_item :: map()
+  @type raw_event :: map()
+  @type canonical_item :: map()
+
+  @callback discover(source(), keyword()) :: {:ok, [discovery_item()]} | {:error, term()}
+  @callback hydrate(source(), discovery_item(), keyword()) :: {:ok, hydrated_item()} | {:error, term()}
+  @callback parse(source(), hydrated_item(), keyword()) :: {:ok, [raw_event()]} | {:error, term()}
+  @callback normalize(source(), raw_event(), keyword()) ::
+              {:ok, canonical_item()} | {:error, term()}
+
+  def resolve(%SourceRegistry{adapter_key: "sec_edgar_forms_v1"}), do: {:ok, SECAdapter}
+  def resolve(%SourceRegistry{adapter_key: nil}), do: :error
+  def resolve(%SourceRegistry{adapter_key: _unknown}), do: :error
+end
