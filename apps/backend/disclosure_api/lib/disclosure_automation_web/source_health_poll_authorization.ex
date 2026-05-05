9404c56bbf45585b46f3bc6cb41ba393610d4c4e
suam4597-ship-it <suam4597@gmail.com>
2026-05-05 16:26:42 +0900

    Add source health poll authorization contract tests (#248)
    * Add source health poll authorization plug
    
    * Gate source health poll route with authorization plug
    
    * Add source health poll authorization contract tests

---
+++ apps/backend/disclosure_api/lib/disclosure_automation_web/source_health_poll_authorization.ex
@@ -0,0 +1,44 @@
+defmodule DisclosureAutomationWeb.SourceHealthPollAuthorization do
+  @moduledoc false
+
+  import Plug.Conn
+  import Phoenix.Controller, only: [json: 2]
+
+  alias DisclosureAutomation.Sources
+  alias DisclosureAutomationWeb.SourceHealthJSON
+
+  @poll_permission "source_health:poll"
+
+  def init(opts), do: opts
+
+  def call(%Plug.Conn{params: %{"source_key" => source_key}} = conn, _opts) do
+    case Sources.get_source_by_key(source_key) do
+      {:ok, _source} ->
+        authorize_poll(conn)
+
+      {:error, :not_found} ->
+        conn
+        |> put_status(:not_found)
+        |> json(SourceHealthJSON.error(%{code: "not_found", message: "source not found"}))
+        |> halt()
+    end
+  end
+
+  def call(conn, _opts), do: authorize_poll(conn)
+
+  defp authorize_poll(conn) do
+    if poll_allowed?(conn.params) do
+      conn
+    else
+      conn
+      |> put_status(:forbidden)
+      |> json(SourceHealthJSON.error(%{code: "forbidden", message: "source poll not allowed"}))
+      |> halt()
+    end
+  end
+
+  defp poll_allowed?(%{"actor_permissions" => permissions}) when is_list(permissions),
+    do: @poll_permission in permissions
+
+  defp poll_allowed?(_params), do: false
+end
